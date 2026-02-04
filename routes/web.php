af935c8628d5d4c4c61b026585f0b7646e67f697
atta <atarik.shen05@gmail.com>
2026-02-04 19:45:43 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Livewire\Dashboard\DashboardIndex;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::livewire('/', DashboardIndex::class)->name('dashboard');
