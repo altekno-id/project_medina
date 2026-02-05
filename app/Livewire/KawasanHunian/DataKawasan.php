230004d1e66fcc2e119f7e9ba6ea2638af95b0d7
atta <atarik.shen05@gmail.com>
2026-02-05 18:37:34 +0700

    Templating

---
+++ app/Livewire/KawasanHunian/DataKawasan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\KawasanHunian;
+
+use Livewire\Component;
+
+class DataKawasan extends Component
+{
+    public function render()
+    {
+        return view('mods.kawasan_hunian.data_kawasan');
+    }
+}
---
+++ resources/views/mods/kawasan_hunian/data_kawasan.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="row g-6">
+        <h5>Data Kawasan</h5>
+    </div>
+</div>
