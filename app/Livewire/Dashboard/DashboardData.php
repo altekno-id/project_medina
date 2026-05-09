f6b3316eb4e9eae9a376540cd9e16270abafa70e
agoengBani <agungbani77@gmail.com>
2026-05-09 23:12:43 +0700

    perbaikan

--- app/Livewire/Dashboard/DashboardData.php
+++ app/Livewire/Dashboard/DashboardData.php
@@ -2,7 +2,12 @@
 
 namespace App\Livewire\Dashboard;
 
-use App\Repositories\DashboardRepo;
+use App\Repositories\MasterBankRepo;
+use App\Repositories\MasterKawasanRepo;
+use App\Repositories\MasterPembangunanUnitRepo;
+use App\Repositories\PermintaanDanaRepo;
+use App\Repositories\PurchaseOrderRepo;
+use App\Repositories\RabRepo;
 use Livewire\Component;
 
 class DashboardData extends Component
@@ -16,14 +21,62 @@ class DashboardData extends Component
 
     public function mount()
     {
-        $data = DashboardRepo::getData();
+        $kawasan = MasterKawasanRepo::getAll();
+        $units = MasterPembangunanUnitRepo::getDt();
+        $rabs = RabRepo::getDt();
+        $banks = MasterBankRepo::getData();
+        $purchaseOrders = PurchaseOrderRepo::getListForView();
+        $permintaanDanas = PermintaanDanaRepo::getData();
 
-        $this->summary = $data['summary'];
-        $this->finance = $data['finance'];
-        $this->progress = $data['progress'];
-        $this->latestPurchaseOrders = $data['latest_purchase_orders'];
-        $this->latestPermintaanDanas = $data['latest_permintaan_danas'];
-        $this->unitTerbaru = $data['unit_terbaru'];
+        $rabList = $rabs->get();
+        $unitList = $units->get();
+
+        $totalRab = $rabList->sum(function ($rab) {
+            return $rab->master_rab_items->sum(function ($item) {
+                return (float) $item->qty_rab * (float) $item->harga_satuan_rab;
+            });
+        });
+
+        $this->summary = [
+            'kawasan' => $kawasan->count(),
+            'unit' => $unitList->count(),
+            'rab' => $rabList->count(),
+            'bank' => $banks->count(),
+            'purchase_order' => count($purchaseOrders),
+            'permintaan_dana' => count($permintaanDanas),
+        ];
+
+        $this->finance = [
+            'total_unit' => $unitList->sum('harga_unit'),
+            'total_rab' => $totalRab,
+            'total_purchase_order' => collect($purchaseOrders)->sum('total_order'),
+            'total_permintaan_dana' => collect($permintaanDanas)->sum('jumlah_permintaan'),
+        ];
+
+        $this->progress = [
+            'total' => 0,
+            'cair' => 0,
+            'belum_cair' => 0,
+            'persen_cair' => 0,
+        ];
+
+        $this->latestPurchaseOrders = array_slice($purchaseOrders, 0, 5);
+        $this->latestPermintaanDanas = array_slice($permintaanDanas, 0, 5);
+        $this->unitTerbaru = MasterPembangunanUnitRepo::getDt()
+            ->latest()
+            ->limit(5)
+            ->get()
+            ->map(function ($unit) {
+                return [
+                    'id' => $unit->id,
+                    'nomor_unit' => $unit->nomor_unit,
+                    'tipe_unit' => $unit->tipe_unit ?? '-',
+                    'kawasan' => $unit->master_kawasans?->nama_master_kawasan ?? '-',
+                    'rab' => $unit->master_rabs?->nama_master_rab ?? '-',
+                    'harga_unit' => $unit->harga_unit,
+                ];
+            })
+            ->toArray();
     }
 
     public function render()
